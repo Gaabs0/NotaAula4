1c6d5cfad98f16a013b573ddaec3a971fc84350a
Gaabs0 <149541458+Gaabs0@users.noreply.github.com>
2024-04-04 15:13:34 -0300

    Delete Exercicio6Java/Exercicio6Java directory

--- Exercicio6Java/Exercicio6Java/src/Pessoa.java
+++
@@ -1,9 +0,0 @@
-public class Pessoa {
-    protected String nome;
-    protected int idade;
-
-    public Pessoa(String nome, int idade) {
-        this.nome = nome;
-        this.idade = idade;
-    }
-}
